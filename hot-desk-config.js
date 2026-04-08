664270115ff48277de6e2658b6e3bd72a4752a3a
PMO-Assistant <pmo-assistant@adco.ie>
2026-04-08 11:47:38 +0100

    Testing new qr code

--- hot-desk-config.js
+++ hot-desk-config.js
@@ -3,5 +3,5 @@
  * Vercel build should overwrite this from env using Frontend/scripts/build-config.js.
  */
 window.HOT_DESK_PUBLIC_ORIGIN = "";
-window.HOT_DESK_API = "https://cabackend.herokuapp.com";
+window.HOT_DESK_API = "https://cabackend-5cfa445b10d2.herokuapp.com/";
 window.HOT_DESK_AUTO_DEV_SESSION = false;

fc32d70d9521167778ab357002cf6d48ececa31f
PMO-Assistant <pmo-assistant@adco.ie>
2026-04-08 12:15:30 +0100

    Set production frontend API URL

--- hot-desk-config.js
+++ hot-desk-config.js
@@ -2,6 +2,6 @@
  * Production-safe defaults.
  * Vercel build should overwrite this from env using Frontend/scripts/build-config.js.
  */
-window.HOT_DESK_PUBLIC_ORIGIN = "";
+window.HOT_DESK_PUBLIC_ORIGIN = "https://ca-01-frontend.vercel.app";
 window.HOT_DESK_API = "https://cabackend-5cfa445b10d2.herokuapp.com";
 window.HOT_DESK_AUTO_DEV_SESSION = false;
